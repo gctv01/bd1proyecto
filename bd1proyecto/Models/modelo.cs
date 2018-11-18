3f60634f65b993eae86e996705387f6483ac6ecc
Giancarlo Tricerri <giankitricerri@hotmail.com>
2018-11-17 23:42:31 -0400

    Agregar archivos de proyecto.

---
+++ bd1proyecto/Models/modelo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bd1.Models
+{
+    public class ModeloRegistro
+    {
+        public int Cedula { get; set; }
+        public string Usuario { get; set; }
+        public int Edad { get; set; }
+        public string Genero { get; set; }
+        public int Contrasena { get; set; }
+    }
+
+    public class ModeloLogIn
+    {
+
+        public string Usuario { get; set; }
+        public int Contrasena { get; set; }
+    }
+}
